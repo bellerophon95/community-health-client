fe5f8e42307bc9c958a4aed8e6ceec0038b1041b
Vibhor Kashmira <vibhor.kashmira@sprinklr.com>
2024-01-14 11:25:40 -0500

    Initial commit

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA7iR8mM4XlYXjodILAt7gNTaDx3X2wYjA",
+  authDomain: "community-health-3173e.firebaseapp.com",
+  projectId: "community-health-3173e",
+  storageBucket: "community-health-3173e.appspot.com",
+  messagingSenderId: "1041341738592",
+  appId: "1:1041341738592:web:fadb491e0e397091dc1d05",
+  measurementId: "G-XVCD57QTRN"
+};
+
+export const vapidKey = 'BHhF4dveQcuBru87q0ZwsNk6jHFCEMwQ7KZZJrI6DEcgQC7OMtKXDcXWTXminsENW5-TqI315RzULYAAgLJHBRQ';
